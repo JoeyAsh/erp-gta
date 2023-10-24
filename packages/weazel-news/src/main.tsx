2fec88ea3f373048639e3be26eae8e0442d1c104
Johannes Aschenbrenner <johannes.aschenbrenner@wechselgott.com>
2023-10-24 11:49:50 +0200

    removed a useless comment

--- packages/weazel-news/src/main.tsx
+++ packages/weazel-news/src/main.tsx
@@ -1,14 +1,12 @@
-import { ThemeProvider } from "@mui/material";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./components/app/App.tsx";
-import "./index.css";
-import darkTheme from "./themes/Theme.ts";
-import { devMode } from "./utils/Config.ts";
+import {ThemeProvider} from '@mui/material';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router} from 'react-router-dom';
+import App from './components/app/App.tsx';
+import './index.css';
+import darkTheme from './themes/Theme.ts';
+import {devMode} from './utils/Config.ts';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 if (window.name === '' || devMode) {
