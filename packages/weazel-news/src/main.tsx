86ca90126b042c393664a85976f5b2e858c813e9
Johannes Aschenbrenner <johannes.aschenbrenner@wechselgott.com>
2023-10-24 10:53:36 +0200

    add lua scripts and wrapped the lb-phone react template

--- packages/weazel-news/src/main.tsx
+++ packages/weazel-news/src/main.tsx
@@ -1,17 +1,35 @@
-import {ThemeProvider} from '@mui/material';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router} from 'react-router-dom';
-import App from './components/app/App.tsx';
-import './index.css';
-import darkTheme from './themes/Theme.ts';
+import { ThemeProvider } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./components/app/App.tsx";
+import "./index.css";
+import darkTheme from "./themes/Theme.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <ThemeProvider theme={darkTheme}>
-            <Router>
-                <App />
-            </Router>
-        </ThemeProvider>
-    </React.StrictMode>
-);
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const devMode = !window?.['invokeNative'];
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+if (window.name === '' || devMode) {
+    const renderApp = () => {
+        root.render(
+            <React.StrictMode>
+                <ThemeProvider theme={darkTheme}>
+                    <Router>
+                        <App />
+                    </Router>
+                </ThemeProvider>
+            </React.StrictMode>
+        );
+    };
+
+    if (devMode) {
+        renderApp();
+    } else {
+        window.addEventListener('message', (event) => {
+            if (event.data === 'componentsLoaded') renderApp();
+        });
+    }
+}
+
